31919751e33f8a850fc29de4f224e87db073293d
Michael <35783820+mib1185@users.noreply.github.com>
2024-04-20 13:52:03 +0200

    Make messages RFC3164 (bsd) compliant (#1)

--- syslog/journal2syslog.py
+++ syslog/journal2syslog.py
@@ -1,12 +1,14 @@
 import logging
 import logging.handlers
 from os import environ
+import re
 import socket
 from systemd import journal
 
 SYSLOG_HOST = str(environ["SYSLOG_HOST"])
 SYSLOG_PORT = int(environ["SYSLOG_PORT"])
 SYSLOG_PROTO = str(environ["SYSLOG_PROTO"])
+HAOS_HOSTNAME = str(environ["HAOS_HOSTNAME"])
 
 # start journal reader and seek to end of journal
 jr = journal.Reader(path="/var/log/journal")
@@ -20,17 +22,28 @@ logger = logging.getLogger("")
 logger.setLevel(logging.NOTSET)
 
 if SYSLOG_PROTO.lower() == "udp":
-    socktype=socket.SOCK_DGRAM
+    socktype = socket.SOCK_DGRAM
 else:
-    socktype=socket.SOCK_STREAM
-logger.addHandler(logging.handlers.SysLogHandler(address=(SYSLOG_HOST, SYSLOG_PORT), socktype=socktype))
+    socktype = socket.SOCK_STREAM
+
+syslog_handler = logging.handlers.SysLogHandler(
+    address=(SYSLOG_HOST, SYSLOG_PORT), socktype=socktype
+)
+formatter = logging.Formatter(
+    f"%(asctime)s %(ip)s %(prog)s: %(message)s",
+    defaults={"ip": HAOS_HOSTNAME},
+    datefmt="%b %d %H:%M:%S",
+)
+syslog_handler.setFormatter(formatter)
+logger.addHandler(syslog_handler)
 
 # wait for new messages in journal
 while True:
     change = jr.wait(timeout=None)
     for entry in jr:
+        extra = {"prog": entry.get("SYSLOG_IDENTIFIER")}
         if "CONTAINER_NAME" in entry:
-            msg = f"{entry.get('__REALTIME_TIMESTAMP')} | {entry.get('SYSLOG_IDENTIFIER')} | {entry.get('MESSAGE')}"
+            msg = re.sub(r"\x1b\[\d+m", "", entry.get("MESSAGE"))
         else:
-            msg = f"{entry.get('__REALTIME_TIMESTAMP')} | host | {entry.get('SYSLOG_IDENTIFIER')} : {entry.get('MESSAGE')}"
-        logger.log(level=entry.get('PRIORITY', logging.INFO), msg=msg)
+            msg = entry.get("MESSAGE")
+        logger.log(level=entry.get("PRIORITY", logging.INFO), msg=msg, extra=extra)
